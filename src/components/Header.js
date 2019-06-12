85808bb3a940cd26ceb148adb248a492743b3cbe
master <swanbaek@naver.com>
2019-06-12 18:18:00 +0900

    Memo App Add01

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Header extends React.Component{
+    render(){
+        return (
+            <div className="jumbotron mt-3">
+            <h1 className="text-primary text-center">React Memo App </h1>
+            </div>
+        );
+    }
+}
+export default Header;
