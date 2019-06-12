114d934063870c4a62d2d957ac573264a0ced667
master <swanbaek@naver.com>
2019-06-12 18:21:48 +0900

    Navbar Add

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+class Navbar extends React.Component{
+    render(){
+        return (
+<nav className="navbar navbar-expand-sm bg-light">
+  <ul className="navbar-nav">
+  <a className="navbar-brand" href="#">
+    <img src="pig.PNG" alt="Logo" style={{width:40}}/>
+  </a>
+    <li className="nav-item active">
+      <a className="nav-link" href="#">Home</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Read</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Create</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Update</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Delete</a>
+    </li>
+  </ul>
+
+</nav>
+        );
+    }
+}
+export default Navbar;
